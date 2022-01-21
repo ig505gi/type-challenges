20f6e7a3da4db89d4debebd6da88da4c6805fdee
Yuan Gao <gaoyuan_seu@163.com>
2022-01-21 16:13:09 +0800

    solu: 189 268 533 898

--- questions/189-easy-awaited/template.ts
+++ questions/189-easy-awaited/template.ts
@@ -1,1 +1,6 @@
-type MyAwaited = any
+type MyAwaited<T extends Promise<any>> =
+  T extends Promise<infer P>
+    ? P extends Promise<any>
+      ? MyAwaited<P>
+      : P
+    : never
--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any;
+import { Equal } from "@type-challenges/utils"
+
+export type Includes<T extends readonly unknown[], U> =
+  T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true
+      ? true
+      : Includes<Rest, U>
+    : false
